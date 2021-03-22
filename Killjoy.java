8b3ea2f2c3a9e5e3223c8e7687d99b50aef481c0
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2021-03-22 21:52:17 +0530

    #013 | Sourav | Kill joy.

---
+++ Killjoy.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Killjoy {
+    public static void main(String[] args) throws Exception {
+        // BufferedReader br = new BufferedReader(new FileReader(new File("input.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        handleMultipleTestCases(br);
+    }
+
+    private static void handleMultipleTestCases(BufferedReader br) throws Exception {
+        int T = readInt(br);
+        for (int tc = 0; tc < T; tc++) {
+            handleTestCase(br);
+        }
+    }
+
+    private static void handleTestCase(BufferedReader br) throws Exception {
+        int[] nx = readArrInt(br);
+        int x = nx[1];
+        int[] A = readArrInt(br);
+        System.out.println(getNumberOfRounds(x, A));
+    }
+
+    private static int readInt(BufferedReader br) throws Exception {
+        return Integer.parseInt(br.readLine());
+    }
+
+    private static int[] readArrInt(BufferedReader br) throws Exception {
+        String[] line = br.readLine().split(" ");
+        int[] arr = new int[line.length];
+        for (int i = 0; i < line.length; i++) {
+            arr[i] = Integer.parseInt(line[i]);
+        }
+        return arr;
+    }
+
+    private static List<Integer> readList(BufferedReader br) throws Exception {
+        String[] line = br.readLine().split(" ");
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < line.length; i++) {
+            arr.add(Integer.parseInt(line[i]));
+        }
+        return arr;
+    }
+
+    private static void print(int[][] mat) {
+        System.out.println("----------------------------------------");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("}");
+        }
+        System.out.println("----------------------------------------");
+    }
+
+    private static void print(int[] mat) {
+        System.out.print("{");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i] + " ");
+        }
+        System.out.println("}");
+    }
+
+    private static int getNumberOfRounds(int x, int[] A) {
+        int diff = 0;
+        int containsX = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == x) {
+                containsX++;
+            }
+            diff = diff + (x - A[i]);
+        }
+        if (containsX == A.length) {
+            return 0;
+        }
+        if (diff == 0 || containsX > 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
